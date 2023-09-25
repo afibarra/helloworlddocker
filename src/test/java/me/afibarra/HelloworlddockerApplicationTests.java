b4c402cfbaea444c49715d9cc0a32833b8e03aed
Armando.Flores <afidroid@gmail.com>
2023-09-24 17:04:18 -0700

    Kick-off

---
+++ src/test/java/me/afibarra/HelloworlddockerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.afibarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworlddockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
